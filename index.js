c10dc91e93b7bbcce0f271398efc9fdd3c9c7eee
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-02-15 12:02:43 -0500

    Due date added, color priority code todos implemented

--- index.js
+++ index.js
@@ -6,40 +6,66 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const task = event.target[0].value;
-  const date = event.target[1].value;
+  const [year, month, day] = event.target[1].value.split("-");
+  const priority = event.target[2].value;
+  // const formattedDateString = `${formattedMonth}-${formattedDay}-${formattedYear}`;
+  let formattedDateString = "";
+
+  if (year && month && day) {
+    const formattedDate = new Date(year, month - 1, day);
+
+    const formattedMonth = String(formattedDate.getMonth() + 1).padStart(
+      2,
+      "0"
+    );
+    const formattedDay = String(formattedDate.getDate()).padStart(2, "0");
+    const formattedYear = formattedDate.getFullYear();
+    formattedDateString = `${formattedMonth}-${formattedDay}-${formattedYear}`;
+  }
 
   const div = document.createElement("div");
-  div.classList.add("li-container");
+  div.classList.add(`li-container`, priority);
+  div.addEventListener("mouseenter", addDeleteBtn);
+
+  div.addEventListener("mouseleave", removeDeleteBtn);
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.addEventListener("change", checkItem);
 
   const li = document.createElement("li");
-
-  div.addEventListener("mouseenter", addDeleteBtn);
-
-  div.addEventListener("mouseleave", removeDeleteBtn);
-  li.innerText = task;
+  li.textContent = task;
 
   div.appendChild(checkbox);
   div.appendChild(li);
+
+  if (formattedDateString) {
+    console.log(formattedDateString);
+    const dateP = document.createElement("p");
+    dateP.classList.add("date");
+    dateP.textContent = `Due Date: ${formattedDateString}`;
+    dateP.style.margin = 0;
+    div.appendChild(dateP);
+  }
 
   todo_ul.appendChild(div);
 
   console.log(event);
 });
-
+debugger;
 function checkItem(event) {
   const parentDiv = event.target.parentElement;
   const li = parentDiv.children[1];
+  const dateP = parentDiv.children[2];
 
   if (event.target.checked) {
     li.style.textDecoration = "line-through";
+    dateP.style.textDecoration = "line-through";
     completed_ul.append(parentDiv);
   } else {
     todo_ul.append(parentDiv);
     li.style.textDecoration = "none";
+    dateP.style.textDecoration = "none";
   }
 }
 
